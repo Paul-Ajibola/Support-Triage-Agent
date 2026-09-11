b6d84a354e58c3f84fde2e32c4b95bd3f17632a9
Paul Ajibola <paulajibola6@gmail.com>
2026-09-11 12:57:52 +0000

    refactored the get_checkpointer() function to stay open (manual: __enter__())

--- agent/checkpointing.py
+++ agent/checkpointing.py
@@ -11,8 +11,9 @@ from langgraph.checkpoint.redis import RedisSaver
 
 def get_checkpointer():
     redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
-    # designed to be used as a context manager and not called directly
+    # designed to be used as a context manager and not callable directly
     cm = RedisSaver.from_conn_string(redis_url)
+    # manually calling the .__enter__(), but ignoring the .__exit__(); to keep it open an active automatically
     checkpointer = cm.__enter__()
     checkpointer.setup()
     return checkpointer
